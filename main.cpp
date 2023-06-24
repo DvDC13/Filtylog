64c19d9ccb9ecf3252b7bc98a817bd57fca7e92f
david.chemaly <davidchemaly1@gmail.com>
2023-06-24 18:59:54 +0200

    feat(filter) : added negative filter

--- main.cpp
+++ main.cpp
@@ -28,6 +28,26 @@ void ApplyGrayscale(unsigned char *imageData, int width, int height)
     }
 }
 
+void ApplyNegative(unsigned char *imageData, int width, int height)
+{
+    for (int i = 0; i < width * height; ++i)
+    {
+        unsigned char r = imageData[i * 3];
+        unsigned char g = imageData[i * 3 + 1];
+        unsigned char b = imageData[i * 3 + 2];
+
+        // Calculate negative value
+        unsigned char negativeR = 255 - r;
+        unsigned char negativeG = 255 - g;
+        unsigned char negativeB = 255 - b;
+
+        // Set negative value to all channels
+        imageData[i * 3] = negativeR;
+        imageData[i * 3 + 1] = negativeG;
+        imageData[i * 3 + 2] = negativeB;
+    }
+}
+
 int main()
 {
     // Initialize GLFW
@@ -78,7 +98,7 @@ int main()
 
         // open Dialog Simple
         if (ImGui::Button("Choose Image"))
-            ImGuiFileDialog::Instance()->OpenDialog("ChooseFileDlgKey", "Choose File", ".png,.jpg", ".");
+            ImGuiFileDialog::Instance()->OpenDialog("ChooseFileDlgKey", "Choose File", ".png", ".");
 
         // display
         if (ImGuiFileDialog::Instance()->Display("ChooseFileDlgKey"))
@@ -115,6 +135,11 @@ int main()
             if (ImGui::Button("Apply Grayscale"))
             {
                 ApplyGrayscale(data, width, height);
+            }
+
+            if (ImGui::Button("Apply Negative"))
+            {
+                ApplyNegative(data, width, height);
             }
         }
 
